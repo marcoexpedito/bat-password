954389cc3b6a3c012d1ca3175ce8b15b745bc5f0
marcoexpedito <marcogomes52@hotmail.com>
2025-07-29 14:33:31 -0300

    fisrt commit

---
+++ src/components/BatButtom/BatButton.tsx
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+import { Text, Pressable } from 'react-native';
+import { styles } from './BatButtonStyle';
+import { BatTextInput } from '../BatTextInput/BatTextInput';
+import generatePass from '../../services/passwordService';
+import * as Clipboard from 'expo-clipboard';
+
+export function BatButtom() {
+  const [pass, setPass] = useState('')
+
+  function handleGenerateButton() {
+    let generateToken = generatePass();
+    setPass(generateToken);
+  }
+
+  function handleCopyButton() {
+    Clipboard.setStringAsync(pass)
+  }
+
+  return (
+    <>
+      <BatTextInput pass={pass}/>
+
+         <Pressable onPress={handleGenerateButton}
+          style = {styles.button}
+          >
+          <Text style= {styles.text}>GENERATE</Text>
+
+
+        </Pressable> 
+
+
+        <Pressable onPress={handleCopyButton}
+          style = {styles.button}>
+          <Text style = {styles.text}> ⚡ COPY </Text>
+
+
+        </Pressable>
+        
+
+    </>
+  );
+}
---
+++ src/screens/Home/Home.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Text, View } from "react-native";
+import { StatusBar } from 'expo-status-bar';
+import styles from './Style';
+import { BatLogo } from "../../components/BatLogo/BatLogo";
+import { BatTextInput } from "../../components/BatTextInput/BatTextInput";
+import { BatButtom } from "../../components/BatButtom/BatButton";
+
+export default function Home() {
+    return(
+        <View style={styles.appContainer}>
+
+          <View style = {styles.logoContainer}>
+             <BatLogo/>
+          </View>
+
+          <View style = {styles.inputContainer}> 
+            <BatButtom/>
+          </View>
+          
+          <StatusBar style="light" />
+        </View>
+    )
+};
